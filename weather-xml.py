5fcc85c092266c85fa793510be07443aafe960b4
michaelgursky <michael.gursky@gmail.com>
2021-06-18 20:58:50 -0400

    Update weather-xml.py
    change temp to temperature

--- weather-xml.py
+++ weather-xml.py
@@ -9,12 +9,12 @@ def csv_append(fn, dt, value):
 if __name__ == "__main__":
   readingFile = "weather-06089.xml"
   tree = parse(readingFile).childNodes[0].childNodes
-  temp = [n for n in tree if n.nodeName == "temperature"][0].getAttribute("value")
+  temperature = [n for n in tree if n.nodeName == "temperature"][0].getAttribute("value")
   humidity = [n for n in tree if n.nodeName == "humidity"][0].getAttribute("value")
   p = [n for n in tree if n.nodeName == "precipitation"][0]
   precipitation = 0 if (p.getAttribute("mode") == "no") else p.getAttribute("value")
   dt = [n for n in tree if n.nodeName == "lastupdate"][0].getAttribute("value")
 
-  csv_append("weather-temp.csv", dt, temp)
+  csv_append("weather-temperature.csv", dt, temperature)
   csv_append("weather-humidity.csv", dt, humidity)
   csv_append("weather-precipitation.csv", dt, precipitation)
